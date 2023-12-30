0f6aa3d4f23074e196fb11aae817a8f6a99f83bd
Mateus Klein <=>
2023-12-30 19:35:54 -0300

    menu hamburguer finalizado

--- js/menu-cel.js
+++ js/menu-cel.js
@@ -1,10 +1,41 @@
 const menuItens = document.querySelector(".menu-cel-itens");
-menuItens.style.display = "none";
+menuItens.style.display = "block";
 
-function abrirMenu(){
-    if(menuItens.style.display == "none"){
-        menuItens.style.display = "block";
-    }else{
-        menuItens.style.display = "none";
+const menuFundo = document.querySelector(".menu-cel-fundo");
+menuFundo.style.display = "none";
+
+const menuBtnFechar = document.querySelector(".menu-cel-btn-fechar");
+menuBtnFechar.style.display = "none";
+
+const menuBtnAbrir = document.querySelector(".menu-cel-btn-abrir");
+menuBtnAbrir.style.display = "block";
+
+function fecharMenu(){
+    if(menuFundo.style.display == "block"){
+        menuFundo.style.display = "none";
+        menuItens.style.marginRight = "-230px";
+        menuBtnFechar.style.display = "none";
+        menuBtnAbrir.style.display = "block";
     }
 }
+
+function abrirMenu(){
+    if(menuFundo.style.display == "none"){
+        menuItens.style.marginRight = "0px";
+        menuFundo.style.display = "block";
+        menuBtnFechar.style.display = "block";
+        menuBtnAbrir.style.display = "none";
+    }else{
+        menuFundo.style.display = "none";
+        menuItens.style.marginRight = "-230px";
+        menuBtnFechar.style.display = "none";
+        menuBtnAbrir.style.display = "block";
+    }
+}
+
+addEventListener("resize", () => {
+    menuFundo.style.display = "none";
+    menuItens.style.marginRight = "-230px";
+    menuBtnFechar.style.display = "none";
+    menuBtnAbrir.style.display = "block";
+});
